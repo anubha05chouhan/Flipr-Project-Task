ee5cedf3a0ff02a581fda3911fab14ecff52e086
anubha05chouhan <129792302+anubha05chouhan@users.noreply.github.com>
2024-08-26 16:48:34 +0530

    Create Product.java

---
+++ models/Product.java
@@ -0,0 +1,85 @@
+package com.yourcompany.models;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+
+    // Default Constructor
+    public Product() {
+    }
+
+    // Parameterized Constructor
+    public Product(int id, String name, String description, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    // Getter for id
+    public int getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+  
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for description
+    public String getDescription() {
+        return description;
+    }
+
+    // Setter for description
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Getter for price
+    public double getPrice() {
+        return price;
+    }
+
+    // Setter for price
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getter for category
+    public String getCategory() {
+        return category;
+    }
+
+    // Setter for category
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
